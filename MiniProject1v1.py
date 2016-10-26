edd87fa58b6bb331b50ff1a9be16c39b574715ff
Jessica Huynh <jhuynh@ualberta.ca>
2016-10-26 16:41:00 -0600

    inserted into db a user

--- MiniProject1v1.py
+++ MiniProject1v1.py
@@ -3,20 +3,30 @@ import hashlib
 import sys
 from userInfo import *
 
-def main():
-	conn = sqlite3.connect('hospital.db')   
+def openConnection():
+	conn = sqlite3.connect('hospital.db') 
+	conn.text_factory = str
 	c = conn.cursor()
 	c.execute('PRAGMA foreign_keys=ON;')
 	
 	scriptFile = open('p1-tables.sql', 'r')
 	script = scriptFile.read()
 	scriptFile.close()
-
 	c.executescript(script)
 
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor()
+
+	return conn, c
+
+
+def closeConnection(conn):
 	conn.commit()
-	
 	conn.close()
+
+def main():
+	conn, c = openConnection()
+	closeConnection(conn)
 
 	sys.stdout.write("Welcome!\n")
 
@@ -43,24 +53,44 @@ def main():
 		addUsers()
 
 def addUsers():
-	userRole = promptForUserRole()
+	role = promptForUserRole()
 	name = promptForName()
-
 	user, pw = promptForLoginInfo()
+
+	addUserSQL(role, name, user, pw)
+
 	sys.stdout.write(user + "\n")
 	sys.stdout.write(pw + "\n")
 
-	# conn = sqlite3.connect('hospital.db')   
-	# c = conn.cursor()
-	# c.execute('PRAGMA foreign_keys=ON;')
+
+def addUserSQL(role, name, user, pw):
+	conn, c = openConnection()
+
+	c.execute("SELECT COUNT(*) FROM staff;")
+	# count = c.fetchall()
+	count = c.fetchone()[0]
+	sys.stdout.write("Count of staff: " + str(count) + "\n");
+
+	insert = [count, role, name, user, pw]
+	c.execute("INSERT INTO staff VALUES (?, ?, ?, ?, ?)", insert)
+	conn.commit()
+
+
+
+	c.execute("SELECT * FROM staff;")
 	
-	# scriptFile = open(file_name.strip(), 'r')
-	# script = scriptFile.read()
-	# scriptFile.close()
-	
-	# c.executescript(script)
-	# conn.commit()
-	# conn.close()
+	print
+	row = c.fetchone()
+	print row
+	print "Column names of staff table:", row.keys()
+	print
+
+	result = c.fetchall()
+	for r in result: #staff_id, role, name, login, password
+	    print "ID: ", row["staff_id"], "Role: ", row["role"], "Name: ", row["name"], "Login: ", row["login"]
+	print
+
+	closeConnection(conn)
 
 if __name__ == "__main__":
 	main()
