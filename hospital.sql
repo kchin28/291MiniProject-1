70feccc07d69273ba4f2d06486272ba2d92f82a8
calho <clho@ualberta.ca>
2016-10-26 19:54:39 -0600

    test Data 2

--- hospital.sql
+++ hospital.sql
@@ -2,8 +2,10 @@ INSERT INTO "charts" VALUES('b1234','a1234','2016-10-16 19:00:00','2016-10-17 19
 INSERT INTO "charts" VALUES('b1235','a1235','2016-10-16 19:00:00','');
 INSERT INTO "charts" VALUES('b1236','a1236','2016-10-16 19:00:00','');
 INSERT INTO "drugs" VALUES('tylenol','pain relief');
+insert into "drugs" values('moltrin', 'pain relief');
 INSERT INTO "inferredallergies" VALUES('tylenol','tums');
 INSERT INTO "patients" VALUES('a1234','Kelly','18-21','West, Edmonton','780-123-4567','780-911-1234');
 INSERT INTO "patients" VALUES('a1235','Jessica','18-21','West1, Edmonton','780-123-4568','780-911-1235');
 INSERT INTO "patients" VALUES('a1236','Calvin','18-21','South, Edmonton','403-123-4567','403-911-4567');
 INSERT INTO "reportedallergies" VALUES('a1234','tylenol');
+insert into "reportedallergies" values('a1235', ,'moltrin');
