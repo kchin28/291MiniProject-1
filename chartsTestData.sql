3c41cb283d2ec6b98082fbeb07a5fe671e72d376
kchin28 <kgchin@ualberta.ca>
2016-10-27 14:09:28 -0700

    Add files via upload

--- chartsTestData.sql
+++ chartsTestData.sql
@@ -1,5 +1,10 @@
 INSERT INTO charts VALUES  ('abcdh','34wsa','1999-03-01','1999-03-07');
 INSERT INTO charts VALUES  ('abcdt','34wsa','1995-03-01','1995-05-07');
-INSERT INTO charts (chart_id,hcno,adate) VALUES  ('abcd4','34wsa','1995-03-02');
+INSERT INTO charts (chart_id,hcno,adate) VALUES  ('abcd4','34wsa','2010-03-02');
 
-INSERT INTO patients VALUES  ('34wsa','Sam A','50-55','dummmy-address','780-980-2345','780-980-2345');
+INSERT into staff VALUES ('qwed2','D','jelly','jelly1','pwJ');
+
+INSERT INTO medications VALUES  ('34wsa','abcd4','qwed2','2010-03-07','2010-03-07','2010-03-09','10mg','drug132');
+
+INSERT INTO symptoms VALUES  ('34wsa','abcd4','qwed2','2010-03-07','excessive coughing');
+INSERT INTO diagnoses VALUES  ('34wsa','abcd4','qwed2','2010-03-07','common cold');
